fca6ee758d03c8320d8b87e5e8dacaaff7efb410
eremeydack <139877570+eremeydack@users.noreply.github.com>
2023-09-22 02:54:48 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,52 @@
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def generate_random_data():
+
+
+
+def main():
+import random
+    return data
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    for item in data:import random
+def main():
+    data = generate_random_data()
+def generate_random_data():
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+    main()
+
+def generate_random_data():
+    return data
+    main()
+import random
+
+    for item in data:
+if __name__ == "__main__":
+    data = generate_random_data()
+def main():
+        print(f"Random Number: {item}")
+
+
+    data = [random.randint(1, 100) for _ in range(10)]    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return data
+
+
+def main():
+def generate_random_data():
+        print(f"Random Number: {item}")
+    for item in data:
+    main()
+if __name__ == "__main__":
+
+    data = generate_random_data()
